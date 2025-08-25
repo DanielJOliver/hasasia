321eaf4c892e8a9a4348761476b2be311278d0c7
Daniel Oliver <mail@danieljoliver.com>
2025-08-25 12:22:13 -0700

    updated functions for Mkkp, Seff, and Sclean

--- hasasia/anisotropy.py
+++ hasasia/anisotropy.py
@@ -12,6 +12,8 @@ import astropy.constants as c
 import itertools as it
 from .sensitivity import GWBSensitivityCurve, DeterSensitivityCurve,  resid_response, get_dt, get_Tspan
 from .utils import strain_and_chirp_mass_to_luminosity_distance
+import hasasia.sensitivity as hsen
+
 
 __all__ = ['SkySensitivity',
            'h_circ',
@@ -50,7 +52,7 @@ class Anisotropy(GWBSensitivityCurve):
         patterns. Only one can be used at a time.
     '''
     def __init__(self, spectra, theta_gw, phi_gw,
-                 pulsar_term=False, pol='gr', iota=None, psi=None, NPIX=8):
+                 pulsar_term=False, pol='gr', iota=None, psi=None, NPIX=12):
         super().__init__(spectra)
         self.pulsar_term = pulsar_term
         self.theta_gw = theta_gw
@@ -58,7 +60,6 @@ class Anisotropy(GWBSensitivityCurve):
         self.pos = - khat(self.thetas, self.phis)
         self.NPIX = NPIX
         self.Tspan = get_Tspan(spectra)
-        # print("First few phis:", self.phis[:5], "thetas:", self.thetas[:5])
 
         if pulsar_term == 'explicit':
             self.pdists = np.array([(sp.pdist/c.c).to('s').value
@@ -105,13 +106,11 @@ class Anisotropy(GWBSensitivityCurve):
         if pol=='gr':
             self.Fplus = np.einsum('ijkl, ijl ->kl', self.D, self.eplus)
             self.Fcross = np.einsum('ijkl, ijl ->kl', self.D, self.ecross)
-                        
-            self.R_IJ = np.zeros((len(self.first), len(self.Fplus[0])))  # Shape: (N_pairs, NPIX)
 
-            for i, (f, s) in enumerate(zip(self.first, self.second)):
-                self.R_IJ[i, :] = self.Fplus[f, :] * self.Fplus[s, :] + self.Fcross[f, :] * self.Fcross[s, :]
-
-            self.R_IJ *= 3 / (2 * self.NPIX)
+            self.R_IJ = (
+                self.Fplus[self.first] * self.Fplus[self.second] +
+                self.Fcross[self.first] * self.Fcross[self.second]
+            ) * (3 / (2 * self.NPIX))
 
         elif pol=='scalar-trans':
             self.Fbreathe = np.einsum('ijkl, ijl ->kl',self.D, self.e_b)
@@ -145,17 +144,17 @@ class Anisotropy(GWBSensitivityCurve):
     #     return self._S_eff
 
     
-    @property
-    def S_eff(self): 
-        """Strain power sensitivity. """
-        if not hasattr(self, '_S_eff'):
-            ii = self.pairs[0]
-            jj = self.pairs[1]
-            kk = np.arange(len(self.chiIJ))
-            num = self.T_IJ[kk] / self.Tspan * self.chiIJ[kk]**2
-            series = num[:,np.newaxis] / (self.SnI[ii] * self.SnI[jj])
-            self._S_eff = np.power(np.sum(series, axis=0),-0.5)
-        return self._S_eff
+    # @property
+    # def S_eff(self): 
+    #     """Strain power sensitivity. """
+    #     if not hasattr(self, '_S_eff'):
+    #         ii = self.pairs[0]
+    #         jj = self.pairs[1]
+    #         kk = np.arange(len(self.chiIJ))
+    #         num = self.T_IJ[kk] / self.Tspan * self.chiIJ[kk]**2
+    #         series = num[:,np.newaxis] / (self.SnI[ii] * self.SnI[jj])
+    #         self._S_eff = np.power(np.sum(series, axis=0),-0.5)
+    #     return self._S_eff
 
     @property
     def S_IJ(self):
@@ -167,28 +166,165 @@ class Anisotropy(GWBSensitivityCurve):
             self._S_IJ =  np.sqrt((self.SnI[ii] * self.SnI[jj]))
         return self._S_IJ
 
-    @property
-    def M_kk(self):
-        """Fisher Matrix for Anisotropy."""
-        if not hasattr(self, '_M_kk'): # Shape (Nfreqs, Npix) -- (IJ, f, k)
-            self._M_kk = np.sum(self.R_IJ[:,np.newaxis,:]**2 * self.T_IJ[:,np.newaxis,np.newaxis] / self.S_IJ[:,:,np.newaxis], axis=0)
-        return self._M_kk
     
-    @property
-    def M_kkp(self):
-        """Fisher Matrix M_{kk', f} for Anisotropy."""
-        if not hasattr(self, '_M_kkp'): # Shape (Nfreqs, Npix, Npix) -- (IJ, f, k, k')
-            self._M_kkp = np.sum(self.R_IJ[:,np.newaxis,:,np.newaxis] * self.R_IJ[:,np.newaxis,np.newaxis,:] * self.T_IJ[:,np.newaxis,np.newaxis,np.newaxis] / self.S_IJ[:,:,np.newaxis,np.newaxis], axis=0)
-        return self._M_kkp
-    
-    @property
-    def S_clean(self):
-        """ Sclean """
-        if not hasattr(self, '_S_clean'): # Shape (Nfreqs, Npix)
-            num = np.sum(self.chiIJ[:,np.newaxis,np.newaxis] * self.T_IJ[:,np.newaxis,np.newaxis] * self.R_IJ[:,np.newaxis,:] / self.S_IJ[:,:,np.newaxis], axis=0)
-            denom = np.sqrt(np.sum(self.T_IJ[:,np.newaxis,np.newaxis] * self.R_IJ[:,np.newaxis,:]**2 / self.S_IJ[:,:,np.newaxis], axis=0))
-            self._S_clean = num / denom
-        return self._S_clean
+    def M_kkp(self, chunk_size=12, diag=False):
+        """
+        Full or diagonal Fisher matrix M_{kk', f} using einsum, with optional chunking.
+        
+        Parameters
+        ----------
+        chunk_size : int or None
+            Size of pixel chunks for block computation. If None, do all at once.
+        diag : bool, default=False
+            If True, compute only the diagonal (shape: Nfreqs x Npix).
+            If False, compute full matrix (shape: Nfreqs x Npix x Npix).
+        
+        Returns
+        -------
+        np.ndarray
+            Fisher matrix.
+        """
+        W_if = self.T_IJ[:, None] / self.S_IJ  # (Npairs, Nfreqs)
+        Nfreqs, Npix = self.S_IJ.shape[1], self.R_IJ.shape[1]
+
+        if diag:
+            # Diagonal only: (Nfreqs, Npix)
+            if chunk_size is None:
+                return np.einsum('if,ik,ik->fk', W_if, self.R_IJ, self.R_IJ, optimize=True)
+            else:
+                M_diag = np.zeros((Nfreqs, Npix))
+                for i in range(0, Npix, chunk_size):
+                    M_diag[:, i:i+chunk_size] = np.einsum(
+                        'if,ik,ik->fk',
+                        W_if,
+                        self.R_IJ[:, i:i+chunk_size],
+                        self.R_IJ[:, i:i+chunk_size],
+                        optimize=True
+                    )
+                return M_diag
+        else:
+            # Full matrix: (Nfreqs, Npix, Npix)
+            M_full = np.zeros((Nfreqs, Npix, Npix))
+            if chunk_size is None:
+                return np.einsum('if,ik,il->fkl', W_if, self.R_IJ, self.R_IJ, optimize=True)
+            else:
+                for i in range(0, Npix, chunk_size):
+                    for j in range(0, Npix, chunk_size):
+                        M_full[:, i:i+chunk_size, j:j+chunk_size] = np.einsum(
+                            'if,ik,il->fkl',
+                            W_if,
+                            self.R_IJ[:, i:i+chunk_size],
+                            self.R_IJ[:, j:j+chunk_size],
+                            optimize=True
+                        )
+                return M_full
+
+    def S_eff_kkp(self, chunk_size=12, diag=False):
+        """
+        Efficient einsum calculation of S_eff_kkp = v_k * M_kkp^-1 * v_kp
+        with optional pixel chunking and diagonal-only mode.
+        
+        Parameters
+        ----------
+        chunk_size : int or None
+            If given, compute S_eff in pixel blocks. Otherwise compute full matrix at once.
+        diag : bool, default=False
+            If True, only compute the diagonal pixel blocks (i == j).
+        
+        Returns
+        -------
+        S_eff : np.ndarray
+            Shape (Nfreqs, Npix, Npix) if diag=False
+            Shape (Nfreqs, Npix) if diag=True
+        """
+        Nfreqs, Npix = self.S_IJ.shape[1], self.R_IJ.shape[1]
+
+        # Precompute weights (Npairs, Nfreqs)
+        W_if = (self.chiIJ[:, None] * self.T_IJ[:, None]) / self.S_IJ
+
+        # v_k and v_kp (Nfreqs, Npix)
+        v_k = np.einsum('if,ik->fk', W_if, self.R_IJ)
+        v_kp = np.einsum('if,il->fl', W_if, self.R_IJ)
+
+        # Get full M_kkp matrix and invert
+        M_kkp_array = self.M_kkp(chunk_size=chunk_size, diag=diag)
+
+        # Invert M_kkp
+        if diag:
+            # Just take reciprocal (element-wise inverse)
+            M_kkp_inv = 1.0 / M_kkp_array  # shape (Nfreqs, Npix)
+            S_eff_diag = np.einsum('fk,fk,fk->fk', v_k, M_kkp_inv, v_kp, optimize=True)
+            S_safe = np.clip(S_eff_diag / self.Tspan, 1e-30, None)
+            return S_safe**(-1/2)
+
+        else:
+            # Full matrix inversion per frequency
+            M_kkp_inv = np.zeros_like(M_kkp_array)
+            for f in range(Nfreqs):
+                M_kkp_inv[f] = np.linalg.inv(M_kkp_array[f])
+
+            # Compute S_eff
+            S_eff = np.zeros((Nfreqs, Npix, Npix))
+            if chunk_size is None:
+                S_eff = np.einsum('fk,fkl,fl->fkl', v_k, M_kkp_inv, v_kp, optimize=True)
+            else:
+                for i in range(0, Npix, chunk_size):
+                    for j in range(0, Npix, chunk_size):
+                        S_eff[:, i:i+chunk_size, j:j+chunk_size] = np.einsum(
+                            'fk,fkl,fl->fkl',
+                            v_k[:, i:i+chunk_size],
+                            M_kkp_inv[:, i:i+chunk_size, j:j+chunk_size],
+                            v_kp[:, j:j+chunk_size],
+                            optimize=True
+                        )
+
+            S_safe = np.clip(S_eff / self.Tspan, 1e-30, None)
+            return S_safe**(-1/2)
+
+    def S_clean(self, A=1e-14, alpha=-2/3):
+        """
+        Compute S_clean = 2 * S_h(f) * Tspan / S_eff
+        using efficient einsum contractions with optimization.
+
+        Parameters
+        ----------
+        A : float
+            Amplitude for S_h(f).
+        alpha : float
+            Spectral index for S_h(f).
+
+        Returns
+        -------
+        S_clean_fk : np.ndarray
+            Shape (Nfreqs, Npix)
+        """
+        # seff_num: Tspan * sqrt(sum_over_pairs( T_IJ * R_IJ^2 / S_IJ ))
+        seff_num = self.Tspan * np.sqrt(
+            np.einsum('i,ik,if->fk',
+                    self.T_IJ,
+                    self.R_IJ**2,
+                    1.0 / self.S_IJ,
+                    optimize=True)
+        )
+
+        # seff_denom: sum_over_pairs( chiIJ * T_IJ * R_IJ / S_IJ )
+        seff_denom = np.einsum('i,i,ik,if->fk',
+                            self.chiIJ,
+                            self.T_IJ,
+                            self.R_IJ,
+                            1.0 / self.S_IJ,
+                            optimize=True)
+
+        S_eff_fk = seff_num / seff_denom
+
+        # S_h_f
+        S_h_f = hsen.S_h(A, alpha, self.freqs)  # shape (Nfreqs,)
+
+        # S_clean_fk: 2 * S_h_f * Tspan / S_eff_fk
+        S_clean_fk = (2.0 * S_h_f[:, None] * self.Tspan) / S_eff_fk
+
+        return S_clean_fk
+
 
     @property
     def Seff_A_aniso(self): 
